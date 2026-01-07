b71d29417c9ae8d2a22d52d0a2df407537830967
Muhammad Saad Amin <saadamin691@gmail.com>
2026-01-08 01:10:13 +0500

    HELLO:

--- React/Reusable Profile Card Component/index.jsx
+++ React/Reusable Profile Card Component/index.jsx
@@ -8,7 +8,6 @@ export function Card({ name, title, bio }) {
     );
 }
 
-
 export function App() {
     const profiles = [
         {
@@ -16,11 +15,24 @@ export function App() {
             name: "Mark",
             title: "Frontend developer",
             bio: "I like to work with different frontend technologies and play video games."
+        },
+        {
+            id: 2,
+            name: "Tiffany",
+            title: "Engineering manager",
+            bio: "I have worked in tech for 15 years and love to help people grow in this industry."
+        },
+        {
+            id: 3,
+            name: "Doug",
+            title: "Backend developer",
+            bio: "I have been a software developer for over 20 years and I love working with Go and Rust."
         }
     ];
+
     return (
         <div className="flex-container">
-            {profiles.map((profile) => (
+            {profiles.map(profile => (
                 <Card
                     key={profile.id}
                     name={profile.name}
